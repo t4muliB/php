7c3e13e27deeab3d438a27131dfdad15de7d79fe
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2025-02-21 14:23:07 +0000

    first trial

--- login.php
+++ login.php
@@ -3,3 +3,11 @@ $dbh = new PDO('mysql:host=localhost;dbname=myphpwebsite', 'db_user', '$mySecret
 $user = $dbh->query('SELECT * FROM `users`', PDO::FETCH_ASSOC);
 
 echo "<h1>Hello ".$_POST["name"]." your submitted password is:".$_POST["pwd"]." </h1>";
+
+foreach ($user as $row){
+    if ($row['username'] == $_POST["username"] && $row['password'] == $_POST["Password:"]){
+        echo "logged in";
+    } else {
+        echo "password incorrect";
+    }
+}
