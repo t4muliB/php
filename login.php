a6ccd0c543757e7605f99aff37565bc2c6815f5b
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2025-02-21 14:24:19 +0000

    second trial

--- login.php
+++ login.php
@@ -5,9 +5,9 @@ $user = $dbh->query('SELECT * FROM `users`', PDO::FETCH_ASSOC);
 echo "<h1>Hello ".$_POST["name"]." your submitted password is:".$_POST["pwd"]." </h1>";
 
 foreach ($user as $row){
-    if ($row['username'] == $_POST["username"] && $row['password'] == $_POST["Password:"]){
+    if ($row['username'] == $_POST["name"] && $row['password'] == $_POST["pwd"]){
         echo "logged in";
-    } else {
-        echo "password incorrect";
-    }
+} else {
+    echo "password incorrect";
+}
 }
