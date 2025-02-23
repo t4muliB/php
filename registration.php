3a732a619e6600e25b9ddd1fd29220c276a8a683
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2025-02-23 12:00:11 +0000

    initial database connection

---
+++ registration.php
@@ -0,0 +1,3 @@
+<?php
+$dbh = new PDO('mysql:host=localhost;dbname=myphpwebsite', 'db_book', '$mySecret4150');
+$result = $dbh->query('INSERT INTO `book`('Username','Password') VALUES ('$username','$password')');
