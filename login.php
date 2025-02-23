6a44f4fdee4d295e02b18b23f18ebefb4c8a353b
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2025-02-23 03:51:53 +0000

    fourth trial

--- login.php
+++ login.php
@@ -4,10 +4,8 @@ $user = $dbh->query('SELECT * FROM `users`', PDO::FETCH_ASSOC);
 
 echo "<h1>Hello ".$_POST["name"]." your submitted password is:".$_POST["pwd"]." </h1>";
 
-foreach ($user as $row){
-    if ($row['username'] == $_POST["name"] && $row['password'] == $_POST["pwd"]){
+foreach ($user as $claimeduser){
+    if ($claimeduser['username'] == $_POST["name"] && $claimeduser['password'] == $_POST["pwd"]){
         echo "logged in";
-} else {
-    echo "wrong password";
-}
+    }
 }
