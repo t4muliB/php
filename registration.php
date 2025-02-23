0a689a347184ac0f8c0b37ec16afbde77b97494c
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2025-02-23 12:13:05 +0000

    typo fix

--- registration.php
+++ registration.php
@@ -1,3 +1,3 @@
 <?php
 $dbh = new PDO('mysql:host=localhost;dbname=myphpwebsite', 'db_book', '$mySecret4150');
-$result = $dbh->query('INSERT INTO `book`('Username','Password') VALUES ('$username','$password')');
+$result = $dbh->query('INSERT INTO `book`("Username","Password") VALUES ($username,$password)');
