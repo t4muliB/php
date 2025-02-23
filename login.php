2758ae2d09869afb53350f7b649e9f8a1d2a339d
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2025-02-23 05:37:51 +0000

    correct variablenames added

--- login.php
+++ login.php
@@ -4,7 +4,6 @@ $records = $dbh->query('SELECT * FROM `users`', PDO::FETCH_ASSOC);
 $claimeduser = $_POST["name"];
 $claimedpassword = $_POST["pwd"];
 
-
 echo "<h1>Hello ".$claimeduser." your submitted password is:".$claimedpassword." </h1>";
 
 foreach ($records as $recorduser){
